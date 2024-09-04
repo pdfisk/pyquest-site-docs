4bf65a7a269bfb0b027c1ad2a57649fdb4987e03
peter fisk <peter.fisk@gmail.com>
2024-09-04 14:33:18 -0400

    app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 function showMarkdown() {
-    let hash = document.location.hash;
-    if (hash.length === 0)
-        hash = '#index';
-    const path = hash.substring(1) + '.md';
-    fetch(path).then(
-        (response) => {
-            console.log('PATH', path);
-            console.log('RESPONSE', response);
-            window.X = response;
-        }
-    )
+    // let hash = document.location.hash;
+    // if (hash.length === 0)
+    //     hash = '#index';
+    // const path = hash.substring(1) + '.md';
+    // fetch(path).then(
+    //     (response) => {
+    //         console.log('PATH', path);
+    //         console.log('RESPONSE', response);
+    //         window.X = response;
+    //     }
+    // )
+    fetch('./index.md').then((response)=>{response.text().then((text)=>{console.log(text)})});
     // if (!response.ok)
     //     throw new Error(`Response status: ${response.status}`);
     // const markdown = await response.text();
