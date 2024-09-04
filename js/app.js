0c24705e35b526be9c573769cb20fec3a71395df
peter fisk <peter.fisk@gmail.com>
2024-09-04 14:22:23 -0400

    fetch then

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
-async function showMarkdown() {
+function showMarkdown() {
     let hash = document.location.hash;
     if (hash.length === 0)
         hash = '#index';
     const path = hash.substring(1) + '.md';
-    const response = await fetch(path);
-    if (!response.ok)
-        throw new Error(`Response status: ${response.status}`);
-    const markdown = await response.text();
-    const converter = new showdown.Converter();
-    const converted_html = document.getElementById('converted_html');
-    converted_html.innerHTML = converter.makeHtml(markdown);
+    fetch(path).then(
+        (response) => {
+            console.log('PATH', path);
+            console.log('RESPONSE', response);
+            window.X = response;
+        }
+    )
+    // if (!response.ok)
+    //     throw new Error(`Response status: ${response.status}`);
+    // const markdown = await response.text();
+    // const converter = new showdown.Converter();
+    // const converted_html = document.getElementById('converted_html');
+    // converted_html.innerHTML = converter.makeHtml(markdown);
 }
